24996f93f9557a50f63a0e66f89824198ed5c67a
Stany Madanu <stanymadhanu.sm@gmail.com>
2024-10-02 17:26:10 +0530

    app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="app d-flex justify-content-center align-items-center">
+    <div className="app d-flex justify-content-center align-items-center overflow-hidden">
       <TodoList />
     </div>
   );
